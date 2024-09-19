f87bbcb4f7dfce54ba52712800ffaec623a8fe58
Jihanchy <habibullahalquaderi2005@gmail.com>
2024-09-19 16:00:22 +0600

    added payoo-bank.js file and login form section

---
+++ js/payoo-bank.js
@@ -0,0 +1,13 @@
+/***step -1*** add event listener in login btn****/
+document.getElementById('login-btn').addEventListener('click', function(event){
+    event.preventDefault();
+
+    const inputNumber = document.getElementById('input-number').value
+    const inputPin = document.getElementById('input-pin').value
+    if(inputNumber==='01613516358' && inputPin==='1557'){
+        console.log('allow user to access this website')
+    }else{
+        alert('wrong phone number or pin')
+    }
+ 
+})
